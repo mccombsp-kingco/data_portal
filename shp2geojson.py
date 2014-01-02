ea47860dacf13b78a569f687bf4bbb9614939dec
mccombsp-kingco <mccombs@imperium.org>
2014-01-02 09:13:34 -0800

    Push result to git repository. Matt K source of code

--- shp2geojson.py
+++ shp2geojson.py
@@ -1,12 +1,12 @@
-import os
+import os, subprocess, time
 
 # identify a source directory of shapefiles
-sourceDir = "/Users/paulmccombs/kccode/KC_Data/utility_SHP/utility/"
+sourceDir = "/Users/paulmccombs/kccode/KC_Data/natres_SHP/natres/"
 
 # test for expected projection print a warning if not expected state plane
 
 # make a list of shape files
-sourceList = ["facility.shp",]
+sourceList = ["agrpddst.shp",]
 
 # identify an output directory
 outputDir = "./" # current directory
@@ -28,3 +28,15 @@ for shapeFile in sourceList:
     print "jsonFileName: " , jsonFileName
     convertString = "ogr2ogr -f geoJSON %s %s"% (jsonFileName, newName)
     os.system(convertString)
+
+# Use suprocess module to push revised data to github.
+# Need to set both the --git-dir and --work-tree
+# http://stackoverflow.com/questions/1386291/git-git-dir-not-working-as-expected
+subprocess.call(['git','--git-dir', outputDir + '/.git',
+                 '--work-tree', outputDir,
+                 'add', jsonFileName])
+subprocess.call(['git', '--git-dir', outputDir + '/.git',
+                 '--work-tree', outputDir,
+                 'commit', '-m', '"Data Upload: ' + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + '"'])
+subprocess.call(['git', '--git-dir', outputDir + '/.git',
+                 '--work-tree', outputDir,'push'])
